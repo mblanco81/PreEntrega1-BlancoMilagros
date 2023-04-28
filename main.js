df953986049c00306ba10217d0c6f2f91fc578fd
mblanco81 <127463754+mblanco81@users.noreply.github.com>
2023-04-27 23:20:16 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-let montoCredito=prompt("Ingrese el monto del crédito a adquirir");
+let montoCredito=prompt(
+
+`1: 100000
+2: 300000
+3: 500000
+4: 700000
+5: 1000000`);
+let plazo=
+
+switch(montoCredito){
+    case valor1:
+      codigo a ejecutar en caso de que el valor sea valor1
+      break;
+    case valor2:
+      codigo a ejecutar en caso de que el valor sea valor2
+      break;
+    .
+    .
+    .
+    default:
+      codigo a ejecutar cuando el valor no es igual a ninguno de los valores anteriores
+      break;
+  }
